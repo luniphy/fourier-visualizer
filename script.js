75955990eacb99e1e2c4c6bc4a065f899733db5e
luniphys_lin <luniphys@gmail.com>
2026-07-06 18:36:42 +0200

    working sliders & responding function

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function setupCanvas(canvas) {
   return ctx;
 }
 
-function setupAxes(canvas, ctx) {
+function setupAxes(canvas, ctx, steps) {
   coordinateLineLength = 3;
   ctx.strokeStyle = "white";
 
@@ -30,37 +30,44 @@ function setupAxes(canvas, ctx) {
   ctx.lineTo(w / 2, h);
   ctx.stroke();
 
-  steps = 15;
-  step = w / 2 / steps;
+  step = w / steps
 
-  // x steps
+  // ticks
   ctx.beginPath();
-  for (let i = 0; i < w / 2; i += step) {
-    // +x
-    ctx.moveTo(w / 2 + i, h / 2 - coordinateLineLength);
-    ctx.lineTo(w / 2 + i, h / 2 + coordinateLineLength);
-    // -x
-    ctx.moveTo(w / 2 - i, h / 2 - coordinateLineLength);
-    ctx.lineTo(w / 2 - i, h / 2 + coordinateLineLength);
-    // +y
-    ctx.moveTo(w / 2 - coordinateLineLength, h / 2 + i);
-    ctx.lineTo(w / 2 + coordinateLineLength, h / 2 + i);
-    // -y
-    ctx.moveTo(w / 2 - coordinateLineLength, h / 2 - i);
-    ctx.lineTo(w / 2 + coordinateLineLength, h / 2 - i);
+  // +/- x
+  for (let x = 0; x < w; x += step) {
+    ctx.moveTo(x , h / 2 + coordinateLineLength);
+    ctx.lineTo(x , h / 2 - coordinateLineLength);
+  }
+  // -y
+  for (let y = h / 2; y < h; y += step) {
+    ctx.moveTo(w / 2 + coordinateLineLength, y);
+    ctx.lineTo(w / 2 - coordinateLineLength, y);
+  }
+  // +y
+  for (let y = h / 2; y >= 0; y -= step) {
+    ctx.moveTo(w / 2 + coordinateLineLength, y);
+    ctx.lineTo(w / 2 - coordinateLineLength, y);
   }
   ctx.stroke();
 }
 
-function func(canvas, ctx, A, f, phi, O) {
+function func(canvas, ctx, steps, A, f, phi, O) {
   ctx.strokeStyle = "white";
 
   w = canvas.clientWidth;
   h = canvas.clientHeight;
 
+  step = w / steps
+
   ctx.beginPath();
   for (let x = 0; x < w; x++) {
-    y = h / 2 - Math.sin(x * 0.05 * f - phi) * (h / 4) * A + O;
+    xCorr = x - w / 2;
+    fCorr = f / step;
+    ACorr = A * step;
+    OCorr = O * step;
+    
+    y = h / 2 - Math.sin((xCorr * fCorr) - phi) * ACorr - OCorr;
 
     if (x === 0) {
       ctx.moveTo(x, y);
@@ -71,16 +78,42 @@ function func(canvas, ctx, A, f, phi, O) {
   ctx.stroke();
 }
 
+function redraw() {
+  A = parseFloat(document.querySelector(".amplitude-slider").value);
+  f = parseFloat(document.querySelector(".frequency-slider").value);
+  phi = parseFloat(document.querySelector(".phase-slider").value);
+  O = parseFloat(document.querySelector(".offset-slider").value);
+
+  document.getElementById("ampVal").textContent = A;
+  document.getElementById("freqVal").textContent = f;
+  document.getElementById("phaseVal").textContent = phi;
+  document.getElementById("offsetVal").textContent = O;
+
+  phi = phi * Math.PI / 180;
+
+  inputCtx.clearRect(0, 0, inputCanvas.width, inputCanvas.height);
+  setupAxes(inputCanvas, inputCtx, stepCount);
+  func(inputCanvas, inputCtx, steps=stepCount, A, f, phi, O);
+}
+
+
+
 inputCanvas = document.getElementById("inputCanvas");
 fourierCanvas = document.getElementById("fourierCanvas");
 
 inputCtx = setupCanvas(inputCanvas);
 fourierCtx = setupCanvas(fourierCanvas);
 
-setupAxes(inputCanvas, inputCtx);
-setupAxes(fourierCanvas, fourierCtx);
+stepCount = 30;
 
-func(inputCanvas, inputCtx, 1, 1, 0, 0);
-func(fourierCanvas, fourierCtx, 1, 1, 0, 0);
+setupAxes(inputCanvas, inputCtx, steps=stepCount);
+setupAxes(fourierCanvas, fourierCtx, steps=stepCount);
+
+redraw();
+
+document.querySelector(".amplitude-slider").oninput = redraw;
+document.querySelector(".frequency-slider").oninput = redraw;
+document.querySelector(".phase-slider").oninput = redraw;
+document.querySelector(".offset-slider").oninput = redraw;
 
 // TODO: Adjust Intro text
